7f9f7dc343c53ba8d68ad464d0fbcb0c3837b599
lofti198 <isolar2005@gmail.com>
2022-09-17 04:37:48 +0300

    selector fixes

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,19 @@ function App() {
     This is the part where the data is read from the dom by checking the url when the new tab is opened.
     This part does not work on the query and list page. (Due to the /maps/place condition.)
   */
-  const source_key: string = 'places_source'; // localstorage query key
-  if (window.location.href.includes('/maps/place/') && window.location.href.includes('listen_to_me')) {
-    const wrapper: any = document.querySelector('div[jstcache][aria-label][role="main"][jsan][data-js-log-root]');
+  const source_key: string = "places_source"; // localstorage query key
+  if (
+    window.location.href.includes("/maps/place/") &&
+    window.location.href.includes("listen_to_me")
+  ) {
+    const wrapper: any = document.querySelector(
+      'div[jstcache][aria-label][role="main"][jsan][data-js-log-root]'
+    );
     const data: any = {};
-    data.Title = wrapper.querySelector('h1').innerText; //Example element queries from the dom on the screen.
-    data.Address = wrapper.querySelector('div.RcCsl.fVHpi.w4vB1d.NOE9ve.M0S7ae.AG25L button').innerText; //Example element queries from the dom on the screen.
+    data.Title = wrapper.querySelector("h1").innerText; //Example element queries from the dom on the screen.
+    data.Address = wrapper.querySelector(
+      "div.RcCsl.fVHpi.w4vB1d.NOE9ve.M0S7ae.AG25L button"
+    ).innerText; //Example element queries from the dom on the screen.
     data.Href = window.location.href; // Current tab location href
 
     const json: any = localStorage.getItem(source_key);
@@ -50,16 +57,22 @@ function App() {
 
             console.log("onclick1");
 
-
             /*
               The part where the names written places are scanned and the links are taken. It is exemplary.
             */
-            const places = document.querySelectorAll('div.Nv2PK.tH5CWc.THOPZb a[jsaction][href*="www.google.com"]');
+            const places = document.querySelectorAll(
+              'div > div[role="article"] > a[jsaction]' //[href*="www.google."]
+            );
+            console.log("places", places);
             places.forEach((link, index) => {
-              const url: any = link.getAttribute('href') + '&listen_to_me';
+              const url: any = link.getAttribute("href") + "&listen_to_me";
               setTimeout(() => {
-                console.log('new tab reading.. left: ' + (places.length - index - 1));
-                chrome.runtime.sendMessage({ create: { url: url, active: false, pinned: true } });
+                console.log(
+                  "new tab reading.. left: " + (places.length - index - 1)
+                );
+                chrome.runtime.sendMessage({
+                  create: { url: url, active: false, pinned: true },
+                });
               }, 2000 * index);
             });
 
@@ -76,11 +89,11 @@ function App() {
               } else {
                 setTimeout(get_places, 1000);
               }
-            }
+            };
             get_places();
           }}
         >
-          Tap1
+          Tap12
         </button>
       </header>
     </div>
